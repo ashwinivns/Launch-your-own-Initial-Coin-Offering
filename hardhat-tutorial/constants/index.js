eadc3b789407fdc1e4e862214912b7f9eee8efd9
ashwini <ashwinikantpandey295>
2022-12-20 19:59:08 +0530

    IcO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xaFE79E80db8F9B8Ab6767C3e8a2c8880056a4a1d";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
